10608d1e6e56136cbc451d94b1d7b918b861ed17
mapogalmagi <133721538+mapogalmagi@users.noreply.github.com>
2025-08-05 21:26:33 +0900

    [level 2] Title: 숫자 블록, Time: 1910.36 ms, Memory: 9.68 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.py
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.py
@@ -9,9 +9,12 @@ def solution(begin, end):
         for i in range(2, int(num ** 0.5) + 1):
             if num % i == 0:
                 # num = i * (num // i)
+                # 조건에서 블럭은 10000000개 이하로 존재한다고 되어있음
+                # (i, num//i) 중에 블럭 100000000개 
                 if num // i <= 10_000_000:
                     block = num // i
                     break
+                # i ≤ 10,000,000이면 그게 ‘사용 가능한 가장 큰 약수’
                 elif i <= 10_000_000:
                     block = i
         answer.append(block)
